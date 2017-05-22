424293392517ae1f8e66d487202b10bda3f9253d
Cory Rylan <splintercode.cb@gmail.com>
2017-05-21 21:14:49 -0500

    refactor: clean up async pokemon detail view

--- src/app/pokemon/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon/pokemon-detail/pokemon-detail.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/do';
 
 import { Pokemon } from './../../common/interfaces/pokemon';
 import { PokemonService } from './../../common/core/services/pokemon.service';
@@ -12,16 +14,21 @@ import { PokemonService } from './../../common/core/services/pokemon.service';
   templateUrl: './pokemon-detail.component.html',
   styleUrls: ['./pokemon-detail.component.scss']
 })
-export class PokemonDetailComponent implements OnInit {
+export class PokemonDetailComponent implements OnInit, OnDestroy {
   pokemon: Observable<Pokemon>;
 
   constructor(
+    private title: Title,
     private router: ActivatedRoute,
     private pokemonService: PokemonService) { }
 
   ngOnInit() {
-    this.pokemon = this.router.params.distinctUntilChanged().mergeMap(params => {
-      return this.pokemonService.pokemon.map(pokemon => pokemon.find(p => p.id === +params['id']));
-    });
+    this.pokemon = this.router.params.distinctUntilChanged()
+      .mergeMap(params => this.pokemonService.pokemon.map(pokemon => pokemon.find(p => p.id === +params.id)))
+      .do(pokemon => this.title.setTitle(`Pokémon #${pokemon.id} ${pokemon.name}`));
+  }
+
+  ngOnDestroy() {
+    this.title.setTitle('Search for Pokémon');
   }
 }
