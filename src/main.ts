05393f14567b4e92f9ff9b0e44e6f4ab2cf9254f
Cory Rylan <splintercode.cb@gmail.com>
2017-05-25 00:05:34 -0500

    refactor: improve SW registration

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,33 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(() => registerServiceWorker());
+
+function registerServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js').then(function (reg) {
+      reg.onupdatefound = function () {
+        const installingWorker = reg.installing;
+
+        installingWorker.onstatechange = function () {
+          switch (installingWorker.state) {
+            case 'installed':
+              if (navigator.serviceWorker.controller) {
+                console.log('New or updated content is available.');
+              } else {
+                console.log('Content is now available offline!');
+              }
+              break;
+
+            case 'redundant':
+              console.error('The installing service worker became redundant.');
+              break;
+          }
+        };
+      };
+    }).catch(function (e) {
+      console.error('Error during service worker registration:', e);
+    });
+  }
+}
