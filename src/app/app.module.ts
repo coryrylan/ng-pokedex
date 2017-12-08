7e9f4e2e14fd3f89bee851c0e2d800d7d4e91f25
coryrylan <splintercode.cb@gmail.com>
2017-12-08 08:21:09 -0600

    feat: upgrade to @angular/service-worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 import { CoreModule } from './common/core/core.module';
 import { SharedModule } from './common/shared/shared.module';
@@ -14,6 +16,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
     CoreModule.forRoot(),
     SharedModule,
     AppRoutingModule
--- src/main.ts
+++ src/main.ts
@@ -12,34 +12,4 @@ platformBrowserDynamic().bootstrapModule(AppModule,
   {
     preserveWhitespaces: false,
     // ngZone: 'noop'
-  })
-  .then(() => registerServiceWorker())
-  .catch(err => console.log(err));
-
-function registerServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js').then(function (reg) {
-      reg.onupdatefound = function () {
-        const installingWorker = reg.installing;
-
-        installingWorker.onstatechange = function () {
-          switch (installingWorker.state) {
-            case 'installed':
-              if (navigator.serviceWorker.controller) {
-                console.log('New or updated content is available.');
-              } else {
-                console.log('Content is now available offline!');
-              }
-              break;
-
-            case 'redundant':
-              console.error('The installing service worker became redundant.');
-              break;
-          }
-        };
-      };
-    }).catch(function (e) {
-      console.error('Error during service worker registration:', e);
-    });
-  }
-}
+  });
